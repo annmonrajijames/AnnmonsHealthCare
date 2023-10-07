4f1d1af62f4e314c96ecca25f9877c5229ea735e
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 00:09:17 +0530

    created a NEW file called RegisterActivity.java and then i had set tv.setOnClickListener() in LoginActivity.java

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -37,5 +37,11 @@ public class LoginActivity extends AppCompatActivity {
 
                 }
         });
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
