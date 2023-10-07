d30871bfa1ba813720c68ad04bee3b7518f5f1f3
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-07 16:18:56 +0530

    First time started playing with .java file

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -3,12 +3,19 @@ package com.example.annmonshealthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+EditText editUsername, editPassword;
+Button btn;
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editUsername=findViewById(R.id.editTextLoginUsername);
+        editPassword=findViewById(R.id.editTextLoginPassword);
     }
 }
