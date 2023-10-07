b28f095c339177badb05e04bb3ff4e4301d4ff80
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-07 23:48:50 +0530

    When the login button is clicked, a method is called etOnClickListener makeText is a static method of the Toast class that creates a new Toast object. getApplicationContext(): Gets the context of the application, which the Toast needs to display itself.

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -3,19 +3,33 @@ package com.example.annmonshealthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-EditText editUsername, editPassword;
-Button btn;
-TextView tv;
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        editUsername=findViewById(R.id.editTextLoginUsername);
-        editPassword=findViewById(R.id.editTextLoginPassword);
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
