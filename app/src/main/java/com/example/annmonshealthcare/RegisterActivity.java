023748af7c89e1343560109f8ef187645cf70a83
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 01:02:11 +0530

    Copy some of those like EditText, button, TextView components from LoginActivity.java But in next step you need change those id's like R.id.editTextLoginUsername to the proper name you provided in the activity_register.xml

--- app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
@@ -3,12 +3,21 @@ package com.example.annmonshealthcare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+EditText edUsername, Edemail, edPassword, edConfirm;
+Button btn;
+TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
     }
 }
