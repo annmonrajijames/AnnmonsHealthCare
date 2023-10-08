63f6f8aabee1496f87c58bd851e10b214c41ab63
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 17:48:02 +0530

    In Database.java, i created methods and constructors. Also imported SQLiteDatabase

--- app/src/main/java/com/example/annmonshealthcare/Database.java
+++ app/src/main/java/com/example/annmonshealthcare/Database.java
@@ -1,4 +1,24 @@
 package com.example.annmonshealthcare;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
