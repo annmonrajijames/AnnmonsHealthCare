e8e6c399d412a6646fc2b7159bf12156f01b9949
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 11:54:37 +0530

    isValid function used, and if it is True then Intent is used where it will redirect the page to LoginActivity.java from RegisterActivity.java
    Also, i beautified edemail to edEmail variable because all other variables are like edUsername, edPassword where the the purpose start with capital letter

--- app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-EditText edUsername, edemail, edPassword, edConfirm;
+EditText edUsername, edEmail, edPassword, edConfirm;
 Button btn;
 TextView tv;
 
@@ -21,7 +21,7 @@ TextView tv;
         setContentView(R.layout.activity_register);
 
         edUsername = findViewById(R.id.editTextRegUsername);
-        edemail = findViewById(R.id.editTextRegemailid);
+        edEmail = findViewById(R.id.editTextRegemailid);
         edPassword = findViewById(R.id.editTextRegPassword);
         edConfirm = findViewById(R.id.editTextRegConfirmPassword);
         btn = findViewById(R.id.buttonReg);
@@ -37,14 +37,17 @@ TextView tv;
             @Override
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
-                String email = edemail.getText().toString();
+                String email = edEmail.getText().toString();
                 String password = edPassword.getText().toString();
                 String confirm = edConfirm.getText().toString();
                 if (username.length() == 0 || email.length() == 0 || password.length() == 0 || confirm.length() == 0) {
                     Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
                 } else {
                     if (password.compareTo(confirm) == 0) {
-
+                          if(isValid(password)) {
+                              Toast.makeText(getApplicationContext(), "Record inserted", Toast.LENGTH_SHORT).show();
+                              startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                          }
                     } else {
                         Toast.makeText(getApplicationContext(), "Password and Confirm password didn't match", Toast.LENGTH_SHORT).show();
 
