901ba371557276d2ae4465f4cc0402109ae13002
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 21:13:54 +0530

    Toast message is not enough, after successful login, it should direct to new page so use Intent

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -32,9 +32,11 @@ public class LoginActivity extends AppCompatActivity {
                 Database db= new Database(getApplicationContext(), "healthcare",null,1);
                 if (username.length()==0 || password.length()==0){
                 Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
+
             } else{
                     if(db.login(username, password)==1) {
                         Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                     }
                     else{
                         Toast.makeText(getApplicationContext(), "Invalid Username and Password", Toast.LENGTH_SHORT).show();
