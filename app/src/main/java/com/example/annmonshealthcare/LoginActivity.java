8ea18a8c7907c730ac9ac2726c02a28a001f1188
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 21:25:02 +0530

    Why SharedPreferences now the code not only checks the login credentials but also saves the successful login's username to SharedPreferences so that you can access it later in other parts of your application.

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.annmonshealthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +38,11 @@ public class LoginActivity extends AppCompatActivity {
             } else{
                     if(db.login(username, password)==1) {
                         Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor= sharedPreferences.edit();
+                        editor.putString("username",username);
+                        //to save our data with Key and Value
+                        editor.apply();
                         startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                     }
                     else{
