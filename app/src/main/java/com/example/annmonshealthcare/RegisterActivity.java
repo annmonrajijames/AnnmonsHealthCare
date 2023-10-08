ff710a02045aec5751684180ee150ce0ddce1cb9
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 11:42:45 +0530

    To check whether the entered password is in appropriate format like in digits, alphabets. Use isValid function. In that function three flags-f1,f2,f3 are set to check. All three flags true means then it password is valid

--- app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/RegisterActivity.java
@@ -35,24 +35,49 @@ TextView tv;
         });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void onClick(View view) {
-            String username=edUsername.getText().toString();
-            String email=edemail.getText().toString();
-            String password=edPassword.getText().toString();
-            String confirm=edConfirm.getText().toString();
-            if(username.length()==0 || email.length()==0 || password.length()==0 || confirm.length()==0){
-                Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
-            }
-            else{
-                if(password.compareTo(confirm)==0){
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String email = edemail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+                if (username.length() == 0 || email.length() == 0 || password.length() == 0 || confirm.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (password.compareTo(confirm) == 0) {
 
-                }else{
-                    Toast.makeText(getApplicationContext(), "Password and Confirm password didn't match", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Password and Confirm password didn't match", Toast.LENGTH_SHORT).show();
 
+                    }
                 }
-            }
 
             }
         });
     }
-}
+        public static boolean isValid(String passwordhere) {
+            int f1=0, f2=0, f3=0;
+            if (passwordhere.length()<0) {
+                return false;
+            } else {
+                for (int p=0; p<passwordhere.length(); p++){
+                    if (Character.isLetter(passwordhere.charAt(p))) {
+                        f1=1;
+                    }
+                }
+                for (int r=0; r<passwordhere.length(); r++){
+                    if (Character.isDigit(passwordhere.charAt(r))) {
+                        f2=1;
+                    }
+                }
+                for (int s=0; s<passwordhere.length(); s++){
+                    if (Character.isLetter(passwordhere.charAt(s))) {
+                        f3=1;
+                    }
+                }
+                if (f1==1 && f2==1 && f3==1)
+                    return true;
+                return false;
+
+            }
+        }
+    }
