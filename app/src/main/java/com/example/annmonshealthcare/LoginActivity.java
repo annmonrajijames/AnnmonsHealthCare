2ba042b24de54d104feffca9b281ced3eb77ec33
Annmon James <84982756+annmonrajijames@users.noreply.github.com>
2023-10-08 21:00:15 +0530

    Database class with object db added in LoginActivity.java, just like you created this statement in RegisterActivity.java

--- app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
+++ app/src/main/java/com/example/annmonshealthcare/LoginActivity.java
@@ -29,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
+                Database db= new Database(getApplicationContext(), "healthcare",null,1);
                 if (username.length()==0 || password.length()==0){
                 Toast.makeText(getApplicationContext(), "Please fill All details", Toast.LENGTH_SHORT).show();
             }
